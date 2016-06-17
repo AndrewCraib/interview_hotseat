62f721c095f7d8bd28bd76d01dae44c51733bd83
Aidan Pinkman <user@CODECLAN19.local>
2016-06-17 15:38:12 +0100

    added employer function

---
+++ client/src/app/employer.js
@@ -0,0 +1,15 @@
+var Employers = function(){
+  this.listOfEmployers = [];
+}
+
+
+Employers.prototype ={
+
+  addEmployers: function(name, logo){
+    this.listOfEmployers.push({
+      employersName: name,
+      employersLogo: logo
+    })
+  }
+
+}
